f6b0c196cc3cd78d55e1076373804f1f10a34777
DimonYarkin <dmitryyarkin@yandex.ru>
2022-08-18 17:06:03 +0300

    первая модификация

--- layouts/default.vue
+++ layouts/default.vue
@@ -8,20 +8,38 @@
       app
     >
       <v-list>
-        <v-list-item
-          v-for="(item, i) in items"
-          :key="i"
-          :to="item.to"
-          router
-          exact
+
+        <v-btn
+          left
+          block
+          depressed
+          elevation="2"
+          plain
+          raised
+          to="/"
         >
-          <v-list-item-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title v-text="item.title" />
-          </v-list-item-content>
-        </v-list-item>
+          <v-icon>
+            {{ icons.mdiHomeOutline }}
+          </v-icon>
+          <div style="padding-left: 10px">Главная</div>
+        </v-btn>
+
+        <v-btn
+          block
+          depressed
+          elevation="2"
+          plain
+          raised
+          to="/login"
+        >
+          <v-icon>
+            {{ icons.mdiAccountOutline }}
+          </v-icon>
+          <div style="padding-left: 10px">Авторизация</div>
+
+        </v-btn>
+
+
       </v-list>
     </v-navigation-drawer>
     <v-app-bar
@@ -29,56 +47,27 @@
       fixed
       app
     >
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-btn
-        icon
-        @click.stop="miniVariant = !miniVariant"
-      >
-        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
-      </v-btn>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"/>
+
       <v-btn
         icon
         @click.stop="clipped = !clipped"
       >
         <v-icon>mdi-application</v-icon>
       </v-btn>
-      <v-btn
-        icon
-        @click.stop="fixed = !fixed"
-      >
-        <v-icon>mdi-minus</v-icon>
-      </v-btn>
-      <v-toolbar-title v-text="title" />
-      <v-spacer />
-      <v-btn
-        icon
-        @click.stop="rightDrawer = !rightDrawer"
-      >
-        <v-icon>mdi-menu</v-icon>
-      </v-btn>
+
+      <v-toolbar-title v-text="title"/>
+      <v-spacer/>
+      <v-spacer/>
+      <v-spacer/>
+      <v-spacer/>
     </v-app-bar>
     <v-main>
       <v-container>
-        <Nuxt />
+        <Nuxt/>
       </v-container>
     </v-main>
-    <v-navigation-drawer
-      v-model="rightDrawer"
-      :right="right"
-      temporary
-      fixed
-    >
-      <v-list>
-        <v-list-item @click.native="right = !right">
-          <v-list-item-action>
-            <v-icon light>
-              mdi-repeat
-            </v-icon>
-          </v-list-item-action>
-          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
+
     <v-footer
       :absolute="!fixed"
       app
@@ -89,29 +78,25 @@
 </template>
 
 <script>
+import {mdiHomeOutline} from '@mdi/js';
+import { mdiAccountOutline } from '@mdi/js';
+
 export default {
   name: 'DefaultLayout',
-  data () {
+  data() {
     return {
       clipped: false,
       drawer: false,
       fixed: false,
-      items: [
-        {
-          icon: 'mdi-apps',
-          title: 'Welcome',
-          to: '/'
-        },
-        {
-          icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire'
-        }
-      ],
+
       miniVariant: false,
       right: true,
       rightDrawer: false,
-      title: 'Vuetify.js'
+      title: 'Vuetify.js',
+      icons: {
+        mdiHomeOutline,
+        mdiAccountOutline
+      }
     }
   }
 }
---
+++ pages/login.vue
@@ -0,0 +1,17 @@
+<template>
+<v-container>
+
+  <h1>Авторизация</h1>
+
+</v-container>
+</template>
+
+<script>
+export default {
+  name: "login"
+}
+</script>
+
+<style scoped>
+
+</style>
